5c39d9eed59fcb09716956b6d926b4c311c83d0c
sarrocks1 <saru10081999@gmail.com>
2021-07-15 00:51:50 +0530

    Adding Test Files

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express');
+var app = express();
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('hello world');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
